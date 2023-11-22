e5ba659f0ff289698f3d33be6994a5663c7d19e2
Alexey Rauheisha <124002546+whoiml@users.noreply.github.com>
2023-11-22 18:04:39 +0300

    Correction due to new namespace

--- Lab5_Definite Integral/Lab5_Definite Integral.cpp
+++ Lab5_Definite Integral/Lab5_Definite Integral.cpp
@@ -2,18 +2,18 @@
 
 int main()
 {
-	double a = 0.0, b = PI;
+	double a = 0.0, b = M_PI;
 	double A = -1, B = 1;
 	double C = -1, D = 1;
 
-	cout << "\n\nE = " << E4 << "\n______________________";
-	cout << "\nTrapezoid Method\n"; solveTrapezoidMethod(a, b, f_v19, E4);
-	cout << "\nSimpson's method\n"; solveSimpsonsMethod(a, b, f_v19, E4);
-	cout << "\nSimpson's Cubature Method\n"; solveSimpsonsCubatureMethod(A, B, C, D, f_v32, E4);
+	cout << "\n\nE = " << precisions::E4 << "\n______________________";
+	cout << "\nTrapezoid Method\n"; solveTrapezoidMethod(a, b, f_v19, precisions::E4);
+	cout << "\nSimpson's method\n"; solveSimpsonsMethod(a, b, f_v19, precisions::E4);
+	cout << "\nSimpson's Cubature Method\n"; solveSimpsonsCubatureMethod(A, B, C, D, f_v32, precisions::E4);
 	
-	cout << "\n\nE = " << E5 << "\n______________________";
-	cout << "\nTrapezoid Method\n"; solveTrapezoidMethod(a, b, f_v19, E5);
-	cout << "\nSimpson's method\n"; solveSimpsonsMethod(a, b, f_v19, E5);
-	cout << "\nSimpson's Cubature Method\n"; solveSimpsonsCubatureMethod(A, B, C, D, f_v32, E5);
+	cout << "\n\nE = " << precisions::E5 << "\n______________________";
+	cout << "\nTrapezoid Method\n"; solveTrapezoidMethod(a, b, f_v19, precisions::E5);
+	cout << "\nSimpson's method\n"; solveSimpsonsMethod(a, b, f_v19, precisions::E5);
+	cout << "\nSimpson's Cubature Method\n"; solveSimpsonsCubatureMethod(A, B, C, D, f_v32, precisions::E5);
 	return 0;
 }
